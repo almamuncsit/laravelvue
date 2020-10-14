b884fce0b8f49ecc6e16279d22b0133c607fa884
Al-Mamun Sarkar <msarkar.cse@gmail.com>
2020-10-14 17:07:30 +0600

    Change frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,23 @@
-import Vue from 'vue';
-import App from './App.vue';
-import './registerServiceWorker';
-import router from './router';
-import store from './store';
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
 
-Vue.config.productionTip = false;
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(axios, VueAxios)
+axios.defaults.headers.common["Authorization"] = "Bearer " + "lasjdf452345345";
+Vue.prototype.$axios = axios;
+Vue.prototype.$api =  function( endpoint ) {
+  return 'http://127.0.0.1:8000/' + endpoint;
+}
+
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
-}).$mount('#app');
+  render: h => h(App)
+}).$mount('#app')
---
+++ frontend/src/views/auth/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1> Login Page </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+
+  created() {
+    this.test();
+  },
+
+  methods: {
+    test() {
+      this.$axios.get(this.$api("api/auth/test")).then((response) => {
+        console.log(response.data)
+      });
+    }
+  },  
+}
+</script>
